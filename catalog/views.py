11fc568d1091261e03bc65a662b1e5188f5d9088
kgmoroz <166741223+kgmoroz@users.noreply.github.com>
2025-05-27 22:32:34 +0300

    добавил views для списка товаров, деталей и корзины

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+    products = Product.objects.filter(id__in=cart.keys())
+    cart_items = [
+        {'product': p, 'quantity': cart[str(p.id)], 'total': p.price * cart[str(p.id)]}
+        for p in products
+    ]
+    total_sum = sum(item['total'] for item in cart_items)
+    return render(request, 'catalog/cart.html', {'cart_items': cart_items, 'total_sum': total_sum})
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    cart[str(product_id)] = cart.get(str(product_id), 0) + 1
+    request.session['cart'] = cart
+    return redirect('catalog:cart')
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    if str(product_id) in cart:
+        del cart[str(product_id)]
+        request.session['cart'] = cart
+    return redirect('catalog:cart')
