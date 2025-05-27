3deb2ddc15e0a4a91cc5255cd18d9b0dc8e67030
kgmoroz <166741223+kgmoroz@users.noreply.github.com>
2025-05-27 18:58:39 +0300

    add CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(DefaultUserAdmin):
+    # здесь можно дополнительно настроить отображаемые столбцы
+    fieldsets = DefaultUserAdmin.fieldsets + (
+        ('Дополнительные поля', {
+            'fields': ('phone', 'address'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'address', 'is_staff')
