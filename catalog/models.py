cd975343b6c36fcc63b47826d1fa48edfd732dec
kgmoroz <166741223+kgmoroz@users.noreply.github.com>
2025-05-27 19:19:44 +0300

    add models and images

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField("название", max_length=200)
+    description = models.TextField("описание", blank=True)
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    image = models.ImageField("изображение", upload_to="products/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
