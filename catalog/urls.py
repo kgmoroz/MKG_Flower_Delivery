38c37f9d25e92b0eca8f19713a0aa5eb2f09aec4
kgmoroz <166741223+kgmoroz@users.noreply.github.com>
2025-05-28 21:15:36 +0300

    feat: URL cart/update/<id>/<action>/

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cart/', views.cart_view, name='cart'),
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/update/<int:product_id>/<str:action>/', views.update_cart, name='update_cart'),
 ]
